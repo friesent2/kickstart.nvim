c3d184f8b19a1585954f5b90a77c10c3fd975ad4
tylerfriesen2 <8469017+tylerfriesen2@users.noreply.github.com>
2026-05-22 21:54:26 -0400

    feat: changed treesitter 'config' to 'configs'

--- lua/custom/plugins/treesitter.lua
+++ lua/custom/plugins/treesitter.lua
@@ -4,7 +4,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
-  main = 'nvim-treesitter.config', -- Sets main module to use for opts
+  main = 'nvim-treesitter.configs', -- Sets main module to use for opts
   -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
   opts = {
     ensure_installed = { 'bash', 'c', 'cpp', 'css', 'diff', 'html', 'json', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
